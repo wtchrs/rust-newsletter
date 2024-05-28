516a91b56d8932b3b52f8ba04484012e871cc1df
wtchrs <wtchr_@hotmail.com>
2024-05-29 01:21:28 +0900

    Add an integration test for /health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ use newsletter_lib::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run().await
+    let listener = std::net::TcpListener::bind("127.0.0.1:8080")?;
+    run(listener)?.await
 }
