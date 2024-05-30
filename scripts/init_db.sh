106da6e50a2d857696e2045aa7280344a87550b0
wtchrs <wtchr_@hotmail.com>
2024-05-30 19:09:18 +0900

    wip: Edit scripts/init_db.sh

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,6 +2,16 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(command -v psql)" ]; then
+  echo >&2 "Error: psql is not installed."
+  exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx is not installed."
+  exit 1
+fi
+
 DB_USER=${POSTGRES_USER:=postgres}
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
@@ -14,3 +24,15 @@ docker run \
   -p "${DB_PORT}":5432 \
   -d postgres \
   postgres -N 1000
+
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  echo >&2 "postgres is still unavailable - sleeping 2 seconds."
+  sleep 2
+done
+
+echo >&2 "postgres is up and running on port ${DB_PORT}."
+
+DATABASE_URL=postgresql://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
+sqlx database create
