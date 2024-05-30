5a6e992d644aed68066f74ebedb591cdc6fc3282
wtchrs <wtchr_@hotmail.com>
2024-05-31 00:18:36 +0900

    wip: Add configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use newsletter_lib::configuration::get_configuration;
 use newsletter_lib::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:8080")?;
+    let settings = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", settings.application_port);
+    let listener = std::net::TcpListener::bind(address)?;
     run(listener)?.await
 }
