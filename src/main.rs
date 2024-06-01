4e3c72014ff25d2c0bef9740b56071b8f48a83e0
wtchrs <wtchr_@hotmail.com>
2024-06-02 00:14:05 +0900

    Add dependency on `secrecy` crate for handling secrets

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use newsletter_lib::configuration::get_configuration;
 use newsletter_lib::startup::run;
 use newsletter_lib::telemetry::{get_subscriber, init_subscriber};
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -10,9 +11,10 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configurations = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configurations.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool =
+        PgPool::connect(configurations.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configurations.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
