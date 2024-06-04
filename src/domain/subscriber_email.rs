adc40e8404b1f50656a42baf4244097171f2fc63
wtchrs <wtchr_@hotmail.com>
2024-06-05 00:41:42 +0900

    Implement validations for `/subscriptions` endpoint

---
+++ src/domain/subscriber_email.rs
@@ -0,0 +1,56 @@
+use validator::ValidateEmail;
+
+#[derive(Debug)]
+pub struct SubscriberEmail(String);
+
+impl SubscriberEmail {
+    pub fn parse(s: String) -> Result<Self, String> {
+        if ValidateEmail::validate_email(&s) {
+            Ok(Self(s))
+        } else {
+            Err("Invalid email address".into())
+        }
+    }
+}
+
+impl AsRef<str> for SubscriberEmail {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use claim::{assert_err, assert_ok};
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "ursula.example.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = "@example.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_domain_is_rejected() {
+        let email = "ursula@".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn valid_email_is_accepted() {
+        let email = "ursula@example.com".to_string();
+        assert_ok!(SubscriberEmail::parse(email));
+    }
+}
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,3 +1,5 @@
+use crate::domain::SubscriberName;
+use crate::domain::{NewSubscriber, SubscriberEmail};
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
@@ -11,17 +13,29 @@ pub struct FormData {
 
 #[tracing::instrument(
     name = "Adding a new subscriber",
-    skip(form, connection_pool),
+    skip(connection_pool, form),
     fields(
         email = %form.email,
         name = %form.name
     )
 )]
 pub async fn subscribe(
-    form: web::Form<FormData>,
     connection_pool: web::Data<PgPool>,
+    form: web::Form<FormData>,
 ) -> HttpResponse {
-    match insert_subscriber(&form, &connection_pool).await {
+    let email = match SubscriberEmail::parse(form.0.email) {
+        Ok(x) => x,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(x) => x,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+
+    let new_subscriber = NewSubscriber { email, name };
+
+    match insert_subscriber(&connection_pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
@@ -29,17 +43,20 @@ pub async fn subscribe(
 
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(form, connection_pool)
+    skip(connection_pool, new_subscriber)
 )]
-async fn insert_subscriber(form: &FormData, connection_pool: &PgPool) -> Result<(), sqlx::Error> {
+async fn insert_subscriber(
+    connection_pool: &PgPool,
+    new_subscriber: &NewSubscriber,
+) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email.as_ref(),
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(connection_pool)
