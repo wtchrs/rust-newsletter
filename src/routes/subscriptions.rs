26ea6b6a4d1fb9b08aa5c07a2e835db3967b923c
wtchrs <wtchr_@hotmail.com>
2024-06-05 20:31:52 +0900

    Refactor `/subscriptions` routing function

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,30 +11,29 @@ pub struct FormData {
     name: String,
 }
 
+impl TryInto<NewSubscriber> for FormData {
+    type Error = String;
+
+    fn try_into(self) -> Result<NewSubscriber, Self::Error> {
+        let email = SubscriberEmail::parse(self.email)?;
+        let name = SubscriberName::parse(self.name)?;
+        Ok(NewSubscriber { email, name })
+    }
+}
+
 #[tracing::instrument(
     name = "Adding a new subscriber",
     skip(connection_pool, form),
-    fields(
-        email = %form.email,
-        name = %form.name
-    )
+    fields(email = %form.email, name = %form.name)
 )]
 pub async fn subscribe(
     connection_pool: web::Data<PgPool>,
     form: web::Form<FormData>,
 ) -> HttpResponse {
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(x) => x,
+    let new_subscriber = match form.0.try_into() {
+        Ok(value) => value,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(x) => x,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-
-    let new_subscriber = NewSubscriber { email, name };
-
     match insert_subscriber(&connection_pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
